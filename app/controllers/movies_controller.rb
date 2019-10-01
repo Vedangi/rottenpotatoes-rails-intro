5ffb4025e5dc7bdbfc217407a3d57a49dd5b95d7
Vedangi Bengali <ec2-user@ip-172-31-27-176.us-east-2.compute.internal>
2019-10-01 19:23:37 +0000

    part2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,11 +12,11 @@ class MoviesController < ApplicationController
 
   def index
      #@movies = Movie.all
-     
+     @all_ratings = Movie.ratings #prt3 
      @sort_column=params[:sort]   #prt2
      @movies=Movie.all.order(@sort_column) #part2
      
-     @all_ratings = Movie.ratings #prt3 
+     
      #params[:ratings].nil ? @var_param = @all_ratings : @var_param = params[:ratings].keys #part3
      #@movies=Movie.where(rating: @var_param).order(@sort_column)   #part3
     
