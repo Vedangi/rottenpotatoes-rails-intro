1b3d8da99df1c398a900009b1ce361c43c7e1e9d
Vedangi Bengali <ec2-user@ip-172-31-27-176.us-east-2.compute.internal>
2019-10-04 01:25:02 +0000

    try7

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
     def self.ratings
-        #Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating}
-        ['G','PG','PG-13','R'].map{|a| a}
+        Movie.select(:rating).distinct.inject([]) { |a, m| a.push m.rating}
+        #['G','PG','PG-13','R'].map{|a| a}
     end    
 end
